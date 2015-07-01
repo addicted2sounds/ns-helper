20c45e5c6f8dd1705627292e89153facfcb0af3e
Oleg <addicted2sounds@gmail.com>
2015-07-01 12:23:49 +0300

    refactored project structure

---
+++ lib/shared/login.rb
@@ -0,0 +1,12 @@
+module ShopOperations
+  module Login
+    include Capybara::DSL
+
+    def login(credentials)
+      visit @site_params[:pages][:login]
+      fill_in 'user_email', with: credentials[:email]
+      fill_in 'user_password', with: credentials[:password]
+      click_button 'Sign in'
+    end
+  end
+end
