dbe926a4a7438a167c145b6e0963d32d30f4c780
Oleg <addicted2sounds@gmail.com>
2015-07-02 09:01:29 +0300

    dump staging method

--- lib/db_helper.rb
+++ lib/db_helper.rb
@@ -3,11 +3,14 @@ require_relative 'settings'
 class DbHelper
   attr_accessor :command_line
   def initialize
-    db = Settings.read('database.yml')[:staging]
-    settings = Settings.read 'settings.yml'
-    cmd = "ssh #{settings[:staging_host]} -C 'pg_dump  -h #{db[:config]} -U#{db[:user]} -p5432 #{db[:database]}' > db.dump"
+    @db = Settings.read('database.yml')[:staging]
+    @settings = Settings.read 'settings.yml'
+
   end
   def dump_staging
-    p CMD_DUMP
+    filename = "dumps/nowshop-#{Time.now.strftime "%d-%m-%Y"}"
+    cmd = "ssh #{@settings[:staging_host]} -C 'pg_dump  -h #{@db[:host]}"\
+      " -U#{@db[:username]} -p5432 #{@db[:database]}' > #{filename}.dump"
+    system cmd
   end
 end
