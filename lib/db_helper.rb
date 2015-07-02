d6a937a3b6f898b7288dc02b03da873c7bab485e
Oleg <addicted2sounds@gmail.com>
2015-07-02 11:11:36 +0300

    clone staging db process

--- lib/db_helper.rb
+++ lib/db_helper.rb
@@ -3,14 +3,23 @@ require_relative 'settings'
 class DbHelper
   attr_accessor :command_line
   def initialize
-    @db = Settings.read('database.yml')[:staging]
+    @db = Settings.read('database.yml')
     @settings = Settings.read 'settings.yml'
 
   end
   def dump_staging
-    filename = "dumps/nowshop-#{Time.now.strftime "%d-%m-%Y"}"
-    cmd = "ssh #{@settings[:staging_host]} -C 'pg_dump  -h #{@db[:host]}"\
-      " -U#{@db[:username]} -p5432 #{@db[:database]}' > #{filename}.dump"
+    filename = "dumps/nowshop-#{Time.now.strftime "%d-%m-%Y"}.dump"
+    cmd = "ssh #{@settings[:staging_host]} -C 'pg_dump -Fc -h #{@db[:staging][:host]}"\
+      " -U#{@db[:staging][:username]} -p5432 #{@db[:staging][:database]}' > #{filename}"
     system cmd
+    filename
+  end
+  def restore_dump(filename)
+    system "dropdb -U postgres #{@db[:dev][:database]}"
+    system "createdb -U postgres -E UTF8 #{@db[:dev][:database]}"
+    system "pg_restore --verbose --clean --jobs 4 -h localhost -U postgres -d #{@db[:dev][:database]} #{filename}"
+  end
+  def clone_staging
+    restore_dump dump_staging
   end
 end
--- test.rb
+++ test.rb
@@ -1,8 +1,9 @@
 require './lib/shop_tester'
 require_relative 'lib/db_helper'
 
-db_helper = DbHelper.new
-db_helper.dump_staging
+# db_helper = DbHelper.new
+# db_helper.clone_staging
+# db_helper.restore_dump 'dumps/nowshop-02-07-2015.dump'
 # helper = ShopHelper.new(:saveur_shop, :dev)
 # helper.setup
 # tester = ShopTester.new(:saveur_shop)
