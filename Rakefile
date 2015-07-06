8c5436ddf316645fc69f9f46da66f98bc99d3feb
addicted <addicted2sounds@gmail.com>
2015-07-06 14:50:31 +0300

    moved config files to separate folder

---
+++ Rakefile
@@ -0,0 +1,6 @@
+require_relative 'lib/shop_helper'
+task default: %w[]
+
+task :setup do |t, arg|
+  p t, arg
+end
--- test.rb
+++
@@ -1,8 +0,0 @@
-require './lib/shop_tester'
-
-
-helper = ShopHelper.new(:saveur_shop, :dev)
-helper.setup
-# tester = ShopTester.new(:saveur_shop)
-# tester.login as: :carrier
-# helper.get_page(:index)
